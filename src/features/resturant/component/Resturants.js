9bc0fbbf3d83f8626eed92354b686179b767dbf2
Yakubu Emmanuel <yakubuemmanuelsimpa@gmail.com>
2023-05-29 04:06:40 +0100

    update

--- src/features/resturant/component/Resturants.js
+++ src/features/resturant/component/Resturants.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
-import { Image } from "react-native";
+import { Image, Text } from "react-native";
 import SvgComponent from "../../../../assets/star";
 
 const Title = styled.Text`
@@ -36,7 +36,11 @@ const RatingWrapper = styled.View`
   justify-content: space-between;
 `;
 
-const OpenImage = styled.Image``;
+const RatingOpen = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
 
 const Address = styled.Text`
   color: ${(props) => props.theme.colors.ui.primary};
@@ -47,12 +51,13 @@ const Address = styled.Text`
 export const ResturantsCard = ({ resturant = {} }) => {
   const {
     name = "Some Restautant",
-    icon,
-    photos = "https://picsum.photos/700",
+    icon = "",
+    photos = "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made.jpg",
+    // photos = "https://picsum.photos/700",
     address = "",
     isOpenNow = true,
     rating = 5,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = resturant;
 
   const starArray = Array.from(new Array(Math.floor(rating)));
@@ -63,18 +68,32 @@ export const ResturantsCard = ({ resturant = {} }) => {
         <Title>{name}</Title>
         <RatingWrapper>
           <Rating>
-            {starArray.map(() => (
-              <SvgComponent />
+            {starArray.map((x, i) => (
+              <SvgComponent key={i + 5} />
             ))}
           </Rating>
-          <Image
-            source={{
-              uri: "https://img.freepik.com/premium-vector/open-sign-board_689076-320.jpg",
-            }}
-            style={{ width: 28, height: 28 }}
-          />
+          <RatingOpen>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red", paddingRight: 12 }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            {isOpenNow && (
+              <Image
+                source={{
+                  uri: "https://img.freepik.com/premium-vector/open-sign-board_689076-320.jpg",
+                }}
+                style={{ width: 28, height: 28 }}
+              />
+            )}
+            <Image
+              source={{
+                uri: icon,
+              }}
+              style={{ width: 15, height: 15 }}
+            />
+          </RatingOpen>
         </RatingWrapper>
-
         <Address>{name}</Address>
       </Info>
     </RestautantCard>
